2f919d89cbd79e053a541fd769c72b66743e0e5d
jonberge1337 <jonbergerandi@gmail.com>
2019-03-14 09:57:54 +0100

    terminado altas.sql

--- altas.sql
+++ altas.sql
@@ -1,9 +1,23 @@
 prompt En que tabla quiere meter datos?
+
 prompt 1: 1. Provincias
 prompt 2: 2. Proveedores
 prompt 3: 3. Catalogo
+prompt 4: 4. Articulos
+prompt 5: 5. Clientes
+prompt 6: 6. Formas de Pago
+prompt 7: 7. Direcciones
+prompt 8: 8. Paises
+prompt 9: 9. Cabecera Pedido
+prompt 10: 10. Detalle Pedido
+prompt 11: 11. Cabecera Albaran
+prompt 12: 12. Detalle Albaran
+prompt 13: 13. Cabecera Factura
+prompt 14: 14. Medidas
+prompt 15: 15. Descuentos
 
-accept selection prompt "Escriba el numero de la tabla"
+
+accept selection prompt "Escriba el numero de la tabla "
 
 set term off
 
@@ -13,7 +27,19 @@ select case '&selection.'
        when '1' then 'altas\altasProvincias.sql'
        when '2' then 'altas\altasProveedores.sql'
        when '3' then 'altas\altasCatalogo.sql'
-       else 'menu'
+       when '4' then 'altas\altasArticulos.sql'
+       when '5' then 'altas\altasClientes.sql'
+       when '6' then 'altas\altasFormPagos.sql'
+       when '7' then 'altas\altasDirecciones.sql'
+       when '8' then 'altas\altasPaises.sql'
+       when '9' then 'altas\altasCabPedido.sql'
+       when '10' then 'altas\altasDetPedido.sql'
+       when '11' then 'altas\altasCabAlbaran.sql'
+       when '12' then 'altas\altasDetAlbaran.sql'
+       when '13' then 'altas\altasCabFactura.sql'
+       when '14' then 'altas\altasMedidas.sql'
+       when '15' then 'altas\altasDescuentos.sql'
+       else 'sqlplus.sql'
        end as script
 from dual;
 
